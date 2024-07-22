f40b30a7b1ac31fa8f99136c8af6a55aef50ca18
ashutoshvish07 <101579327+ashutoshvish07@users.noreply.github.com>
2024-07-23 00:39:41 +0530

    deploy again

--- src/index.jsx
+++ src/index.jsx
@@ -31,7 +31,6 @@ import { store } from 'Redux/store';
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 
-// ==============================|| REACT DOM RENDER  ||============================== //
 
 root.render(
   <Provider store={store}>
